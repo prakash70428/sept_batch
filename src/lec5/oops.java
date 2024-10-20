37d810598a5a4ec37edbac0d7f8e232e77731a31
prakash70428 <prakashmendish@gmail.com>
2024-10-20 19:54:58 +0530

    lecture 5 updated

---
+++ src/lec5/oops.java
@@ -0,0 +1,42 @@
+package lec5;
+
+public class oops {
+	public static class Person{
+		String name;
+		int age;
+		
+		public void sayHi() {
+			System.out.println(name + "@" + age + " Says Hi");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Person p1 = new Person();
+        p1.name = "A";
+        p1.age = 10;
+        
+        Person p2 = new Person();
+        p2.name = "B";
+        p2.age = 20;
+        
+        p1.sayHi();
+        p2.sayHi();
+        swap1(p1,p2);
+        p1.sayHi();
+        p2.sayHi();
+	}
+	public static void swap(Person p1,Person p2) {
+		Person temp = p1;
+		p1 = p2;
+		p2 = temp;		
+	}
+    public static void swap1(Person p1,Person p2) {
+    	String t = p1.name;
+    	p1.name = p2.name;
+    	p2.name = t;
+    	
+    	int temp = p1.age;
+    	p1.age = p2.age;
+    	p2.age = temp;
+    }
+}
